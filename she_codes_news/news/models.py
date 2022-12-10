7a7fb5d4b4d8e4f13ed374fac601531f662c084a
AnnieJDesigns <114784081+AnnieJDesigns@users.noreply.github.com>
2022-12-11 10:02:49 +1100

    Created News Site
    Created the news site, added in login function, create new story and add in image.

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+USER = get_user_model()
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    # author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        USER, on_delete=models.CASCADE
+    )
     pub_date = models.DateTimeField()
     content = models.TextField()
+
+    class Meta:
+        ordering = ['pub_date']
+
+    #Add image field
+    image_field = models.URLField(max_length=200, null=True)
+
+
+    
+    
+
+
+
